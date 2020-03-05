e1af34feae38bd39a9dd489aeae78671d245cba8
Jason Whetton <jason@digital-arkitekt.se>
2020-03-05 17:31:15 +0100

    started implementation

---
+++ Lab01/src/Lab01.Domain.Tests/PlaylistTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Xunit;
+using Lab01.Domain;
+using System.Linq;
+
+namespace Lab01.Domain.Tests
+{
+    public class PlaylistTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var playlist = new Playlist(string.Empty);
+
+            Assert.NotNull(playlist.Title);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var playlist = new Playlist(string.Empty);
+
+            Assert.True(playlist.IsActive);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var playlist = new Playlist(string.Empty);
+
+            Assert.NotEmpty(playlist.Title);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            var playlist = new Playlist(string.Empty);
+
+            playlist.Songs.Add(new Playlist.Song());
+
+            Assert.True(playlist.Songs.Count() > 0);
+        }
+    }
+}
---
+++ Lab01/src/Lab01.Domain/Playlist.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab01.Domain
+{
+    public class Playlist
+    {
+        public List<Song> Songs {get;set;}
+        
+        public Playlist(string title) {
+            IsActive = true;
+            this.Title = "abc" + title;
+            Songs = new List<Song>();
+            //
+        }
+
+        public string Title {get;set;}
+
+        public bool IsActive {get;set;}
+
+        public class Song {
+
+        }
+    }
+}
