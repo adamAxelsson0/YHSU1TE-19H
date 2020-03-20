8db2ca0a7119dbf730a1a20b6f646139da61b678
Digital Arkitekt <jason@digital-arkitekt.se>
2020-03-20 13:42:47 +0100

    Update create_project.sh

--- Lab03/create_project.sh
+++ Lab03/create_project.sh
@@ -2,6 +2,6 @@ dotnet new sln
 mkdir src
 dotnet new classlib -o src/Lab03.Domain
 dotnet new xunit -o src/Lab03.Domain.Tests
-dotnet add src/Lab03.Domain.Tests/Lab03.Tests.csproj reference src/Lab03.Domain/Lab03.Domain.csproj
+dotnet add src/Lab03.Domain.Tests/Lab03.Domain.Tests.csproj reference src/Lab03.Domain/Lab03.Domain.csproj
 dotnet sln add src/Lab03.Domain/Lab03.Domain.csproj
 dotnet add src/Lab03.Domain.Tests/Lab03.Domain.Tests.csproj reference src/Lab03.Domain/Lab03.Domain.csproj 
