eb5d0b8d312032e31552b11424594fe174a8114c
Jason Whetton <jasonwhetton@Jasons-MBP.lan>
2020-03-21 09:55:56 +0100

    added results

---
+++ Lab03/src/Lab03.Domain.Tests.Moq/BookingConfirmTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Xunit;
+using Moq;
+using Autofac.Extras.Moq;
+
+namespace Lab03.Domain.Confirm.Tests
+{
+    public class BookingServiceTests
+    {
+        //[Fact]
+        //public void Booking_confirm_should_take_a_payment()
+        //{
+        //    // arrange
+        //    var paymentGateway = new Mock<IPaymentGateway>();
+        //    var priceCalculator = new Mock<IPriceCalculator>();
+        //    var booking = new Booking(paymentGateway.Object, priceCalculator.Object);
+
+        //    // act
+        //    booking.Confirm1();
+
+        //    // assert
+        //    paymentGateway.Verify(s => s.CapturePayment(It.IsAny<decimal>(), It.IsAny<decimal>()), Times.Once);
+
+        //}
+
+        //[Fact]
+        //public void Booking_confirm_should_take_two_payments_with_amount_50()
+        //{
+        //    // arrange
+        //    var paymentGateway = new Mock<IPaymentGateway>();
+        //    var priceCalculator = new Mock<IPriceCalculator>();
+        //    var booking = new Booking(paymentGateway.Object, priceCalculator.Object);
+
+        //    // act
+        //    booking.Confirm2();
+
+        //    // assert
+        //    paymentGateway.Verify(s => s.CapturePayment(50, It.IsAny<decimal>()), Times.Exactly(2));
+        //}
+
+        //[Fact]
+        //public void Booking_confirm_should_take_one_payment_with_amount_100_and_any_vat()
+        //{
+        //    // arrange
+        //    var paymentGateway = new Mock<IPaymentGateway>();
+        //    var priceCalculator = new Mock<IPriceCalculator>();
+        //    var booking = new Booking(paymentGateway.Object, priceCalculator.Object);
+
+        //    // act
+        //    booking.Confirm3();
+
+        //    // assert
+        //    paymentGateway.Verify(s => s.CapturePayment(100, It.IsAny<decimal>()), Times.Once);
+        //}
+
+        //[Fact]
+        //public void Booking_should_take_one_payment_with_amount_50_and_another_with_amount_65()
+        //{
+        //    // arrange
+        //    var paymentGateway = new Mock<IPaymentGateway>();
+        //    var priceCalculator = new Mock<IPriceCalculator>();
+        //    var booking = new Booking(paymentGateway.Object, priceCalculator.Object);
+
+        //    // act
+        //    booking.Confirm4();
+
+        //    // assert
+        //    paymentGateway.Verify(s => s.CapturePayment(50, It.IsAny<decimal>()), Times.Once);
+        //    paymentGateway.Verify(s => s.CapturePayment(65, It.IsAny<decimal>()), Times.Once);
+        //}
+
+        //[Fact]
+        //public void Booking_confirm_should_use_the_amount_suggested_by_the_price_calculator()
+        //{
+        //    // arrange
+        //    var paymentGateway = new Mock<IPaymentGateway>();
+        //    var priceCalculator = new Mock<IPriceCalculator>();
+        //    var booking = new Booking(paymentGateway.Object, priceCalculator.Object);
+        //    var amount = 50m;
+        //    var vatAmount = 2.5m;
+
+        //    priceCalculator.Setup(s => s.CalculatePrice()).Returns(new Price { Amount = amount, VatAmount = vatAmount });
+
+        //    // act
+        //    booking.Confirm5();
+
+        //    // assert
+        //    paymentGateway.Verify(s => s.CapturePayment(amount,vatAmount), Times.Once);
+        //}
+
+
+        [Fact]
+        public void Booking_confirm_should_take_two_different_payments_suggested_by_the_price_calculator()
+        {
+            // arrange
+            using var mock = AutoMock.GetLoose();
+
+            var booking = mock.Create<Booking>();
+            var priceCalculator = mock.Mock<IPriceCalculator>();
+            var paymentGateway = mock.Mock<IPaymentGateway>();
+
+            var price1 = new Price { Amount = 100, VatAmount = 5m };
+            var price2 = new Price { Amount = 50, VatAmount = 2.5m };
+
+            priceCalculator.SetupSequence(s => s.CalculatePrice())
+                .Returns(price1)
+                .Returns(price2);
+
+            // act
+            booking.Confirm6();
+
+            // assert
+            paymentGateway.Verify(s => s.CapturePayment(price1.Amount, price1.VatAmount), Times.Once);
+            paymentGateway.Verify(s => s.CapturePayment(price2.Amount, price2.VatAmount), Times.Once);
+        }
+    }
+}
--- Lab03/src/Lab03.Domain/Booking.cs
+++ Lab03/src/Lab03.Domain/Booking.cs
@@ -1,4 +1,75 @@
 ﻿namespace Lab03.Domain.Confirm
 {
+    public interface IPaymentGateway {
+        bool CapturePayment(decimal amount, decimal vatAmount);
+    }
 
+    public class Price {
+        public decimal Amount {get;set;}
+        public decimal VatAmount {get;set;}
+    }
+
+    public interface IPriceCalculator {
+        Price CalculatePrice();
+    }
+
+    public interface ILogger {
+        void LogSomething(string something);
+    }
+    public class Booking
+    {
+        private readonly IPaymentGateway paymentGateway;
+        private readonly IPriceCalculator priceCalculator;
+
+        public Booking(IPaymentGateway paymentGateway, IPriceCalculator priceCalculator)
+        {
+            this.paymentGateway = paymentGateway;
+            this.priceCalculator = priceCalculator;
+        }
+
+        public void Confirm1()
+        {
+            this.paymentGateway.CapturePayment(50, 50);
+        }
+
+        public void Confirm2()
+        {
+            this.paymentGateway.CapturePayment(50, 50); 
+            this.paymentGateway.CapturePayment(50, 50);
+        }
+
+        public void Confirm3()
+        {
+            this.paymentGateway.CapturePayment(100, 0);
+        }
+
+        public void Confirm4()
+        {
+            this.paymentGateway.CapturePayment(65, 0);
+            this.paymentGateway.CapturePayment(50, 0);
+        }
+
+        public void Confirm5()
+        {
+            var result = priceCalculator.CalculatePrice();
+
+            this.paymentGateway.CapturePayment(result.Amount, result.VatAmount);
+        }
+
+        public void Confirm6()
+        {
+            var result1 = priceCalculator.CalculatePrice();
+
+            this.paymentGateway.CapturePayment(result1.Amount, result1.VatAmount);
+
+            var result2 = priceCalculator.CalculatePrice();
+
+            this.paymentGateway.CapturePayment(result2.Amount, result2.VatAmount);
+        }
+
+        public void Confirm7()
+        {
+            this.paymentGateway.CapturePayment(50, 50);
+        }
+    }
 }
