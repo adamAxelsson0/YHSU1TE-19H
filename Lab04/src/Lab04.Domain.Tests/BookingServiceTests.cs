550413d3133b149a6a3ba4e0b96971f07fec4d15
Jason Whetton <jasonwhetton@Jasons-MacBook-Pro.local>
2020-03-27 14:21:07 +0100

    fixed

--- Lab04/src/Lab04.Domain.Tests/BookingServiceTests.cs
+++ Lab04/src/Lab04.Domain.Tests/BookingServiceTests.cs
@@ -14,14 +14,14 @@ namespace Lab04.Domain.Tests
         {
             // arrange
             var id = Guid.NewGuid().ToString();
-            var bookingRepository = new BookingRepositoryMongoDB(mongoClient);
+            var bookingRepository = new BookingRepositoryMongoDB(this.mongoClient);
             // unmanaged dependency - interface
             var paymentGateway = A.Fake<IPaymentGateway>();
             var priceCalculator = A.Fake<IPriceCalculator>();
             var sut = new BookingService(bookingRepository, paymentGateway, priceCalculator);
 
             // act
-            //sut.CreateBooking(new BookingService.CreateBookingRequest(id, "jason"));
+            sut.CreateBooking(new BookingService.CreateBookingRequest(id, "jason"));
 
             var booking = bookingRepository.GetById(id);
 
@@ -32,13 +32,10 @@ namespace Lab04.Domain.Tests
         //[Fact] // behavioural or communication (white box)
         public void Successful_booking_should_capture_payment()
         {
-             var settings = MongoClientSettings.FromConnectionString("mongodb://localhost:27017");
-             var mongoClient = new MongoClient(settings);
-
             // arrange
             var id = Guid.NewGuid().ToString();
             // managed dependency - concrete instance
-            var bookingRepository = new BookingRepositoryMongoDB(mongoClient);
+            var bookingRepository = new BookingRepositoryMongoDB(this.mongoClient);
             // unmanaged dependency - interface
             var priceCalculator = A.Fake<IPriceCalculator>();
             var paymentGateway = A.Fake<IPaymentGateway>();
@@ -55,13 +52,11 @@ namespace Lab04.Domain.Tests
         //[Fact] // behavioural or communication (white box)
         public void Should_use_total_price_from_price_service()
         {
-             var settings = MongoClientSettings.FromConnectionString("mongodb://localhost:27017");
-             var mongoClient = new MongoClient(settings);
             // arrange
             var auto = new AutoFake();
             var priceCalculator = auto.Resolve<IPriceCalculator>();
             var paymentGateway = auto.Resolve<IPaymentGateway>();
-            auto.Provide(mongoClient);
+            auto.Provide(this.mongoClient);
 
             var id = Guid.NewGuid().ToString();
 
